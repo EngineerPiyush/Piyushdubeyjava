58164240e3b4692cfed9563c376db9cad2adc45d
Piyush Dubey <89024653+Sirjee123@users.noreply.github.com>
2021-10-11 19:21:30 +0530

    Add files via upload

---
+++ IFELSE.java
@@ -0,0 +1,20 @@
+ class IFELSE {
+ 
+    public static void main(String[] args) {
+         
+        int a=10;
+         
+        if (a<=5)
+        {
+             
+            System.out.println("a is less than 5");
+             
+        } else
+               {
+             
+                       System.out.println("a is greater than 5");
+        }
+ 
+    }
+ 
+}
